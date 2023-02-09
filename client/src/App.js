1a955edb963022a4883a0b3de5b5ed54d1d48e14
OljaGleyzer <o.gleyzer@gmail.com>
2023-02-09 13:36:28 +0100

    implementing navigation bar: responsive, BurgerNavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./views/Home";
 import { PlantsContextProvider } from "./store/PlantsContext";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <div>
+      <NavigationBar />
       <PlantsContextProvider>
         <Home />
       </PlantsContextProvider>
---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,34 @@
+import "./NavBar.css";
+
+function NavigationBar() {
+  return (
+    <>
+      <header className="nav-bar">
+        <a href="/" className="logo">
+          {" "}
+          Garden App{" "}
+        </a>
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="navicon"></span>
+        </label>
+        <ul className="menu">
+          <li>
+            {" "}
+            <a href="/">plants</a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="https://www.google.com">log in</a>
+          </li>
+          <li>
+            {" "}
+            <a href="https://www.google.com">sign up</a>{" "}
+          </li>
+        </ul>{" "}
+      </header>{" "}
+    </>
+  );
+}
+
+export default NavigationBar;
