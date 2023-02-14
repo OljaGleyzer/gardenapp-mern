1a8c6b0d7b6c24ed544f939c69ad0b2d2876723f
OljaGleyzer <o.gleyzer@gmail.com>
2023-02-14 14:11:20 +0100

    fixed display error pdp

--- client/src/views/Pdp.js
+++ client/src/views/Pdp.js
@@ -1,34 +1,40 @@
 import { Navigate, useParams } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import useFetch from "../hooks/useFetch";
+import { useEffect, useState } from "react";
 
 function Pdp() {
   const { _id } = useParams();
   console.log("useParams()", useParams());
   const { plant, error, isLoading } = useFetch(_id); // add plant to pass it from useFetch but an array
   console.log("plant", plant);
-  // const plantById = data && data.plantById ? data.plantById : [];
-  // const plant = data.plantById.length > 0 ? data.plantById[0] : null; //plantbyId and data not deifined
+  const [selectedPlant, setSelectedPlant] = useState(null);
+
+  useEffect(() => {
+    if (plant) {
+      setSelectedPlant(plant.plantById[0]);
+    }
+  }, [plant]);
 
   return (
     <div>
       {isLoading ? (
         <p>Loading...</p>
-      ) : plant ? (
+      ) : selectedPlant ? (
         <>
           <div className="container">
-            <h1 className="text-center"> {plant.plantByID}</h1>
+            <h1 className="text-center"> {selectedPlant.name}</h1>
             <div className="product-img-container">
               <img
                 className="container d-flex justify-content-center"
-                src={plant.image}
+                src={selectedPlant.image}
                 style={{ height: "30em", width: "auto" }}
               ></img>
             </div>
             <p></p>
-            <p>Description: {plant.description}</p>
-            <h5>Germinating Season: {plant.germinating_season} €</h5>
-            <h5>Harvest months: {plant.harvest} €</h5>
+            <p>Description: {selectedPlant.description}</p>
+            <h5>Germinating Season: {selectedPlant.germinating_season} €</h5>
+            <h5>Harvest months: {selectedPlant.harvest} €</h5>
             <div className=" align-items-baseline">
               <Button variant="primary">Save Me</Button>
             </div>
