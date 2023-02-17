5b4d59b7d172e784745003e4da0e4beb0470e0f1
OljaGleyzer <o.gleyzer@gmail.com>
2023-02-17 10:16:31 +0100

    Logout function, remove token

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,11 +1,57 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { getToken } from "../utils/getToken";
+import { useEffect } from "react";
 
-const Login = () => {
+function Login() {
   const [loginUser, setLoginUser] = useState(null);
   const handleChangeHandler = (e) => {
     setLoginUser({ ...loginUser, [e.target.name]: e.target.value });
+    // console.log("loginUser", loginUser);
   };
+  const login = () => {
+    // Check email format, password lenght ...avoid making useless requests to the server
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("email", loginUser.email);
+    urlencoded.append("password", loginUser.password);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:5000/api/users/login", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.token) {
+          console.log(result.token);
+          localStorage.setItem("token", result.token);
+          setLoginUser(result.user);
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+  const logout = () => {
+    localStorage.removeItem("token");
+    setLoginUser(null);
+  };
+  useEffect(() => {
+    const token = getToken();
+
+    if (token) {
+      console.log("LOGGED IN");
+    } else {
+      console.log("NOT logged in");
+    }
+  }, [loginUser]);
+
   return (
     <div className="login">
       {/*    {newUser.email ? <h1>Hello {user.email}</h1> : */}
@@ -23,7 +69,7 @@ const Login = () => {
         // value={password}
         type="password"
         id="password"
-        name="pasword"
+        name="password"
         placeholder="Password"
         onChange={handleChangeHandler}
       />
@@ -35,7 +81,7 @@ const Login = () => {
       ) : null} */}
       <button
         className="register-button"
-        // onClick={fetchSignUp}
+        onClick={login}
         // disabled={
         //   newUser.password.length < 6 ||
         //   !newUser.email.includes("@") ||
@@ -49,8 +95,11 @@ const Login = () => {
       <Link to="/signup">
         <button className="login-button">No Acount? Sign Up!</button>
       </Link>
+      <button className="register-button" onClick={logout}>
+        Logout
+      </button>
     </div>
   );
-};
+}
 
 export default Login;
