adbf4b67ba53dac254a3b1d11b2358b03254e12e
OljaGleyzer <o.gleyzer@gmail.com>
2023-03-06 11:19:23 +0100

    delete comment function frontend done

--- client/src/views/Pdp.js
+++ client/src/views/Pdp.js
@@ -2,8 +2,6 @@ import { Link, Navigate, useParams } from "react-router-dom";
 import { Button, Modal } from "react-bootstrap";
 import useFetch from "../hooks/useFetch";
 import { useContext, useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { AuthContext } from "../store/AuthContext";
 import { getToken } from "../utils/getToken";
 
@@ -21,10 +19,24 @@ function Pdp() {
     authorPicture: "",
   });
   const [inputText, setInputText] = useState("");
-  // const [updatedComments, setUpdatedComments] = useState([]);
+  const [updatedComments, setUpdatedComments] = useState({});
+  const [commentAuthor, setCommentAuthor] = useState("");
 
   const { loggedinUser } = useContext(AuthContext);
   console.log("%cloggedinUser", "color:orange", loggedinUser); //FIXME - loggedinUser is undefined why?
+
+  // const findCommentAuthor = () => {
+  //   selectedPlant.comments.map((comment) => {
+  //     setCommentAuthor(...commentAuthor, comment.author);
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   findCommentAuthor();
+  // }, []);
+
+  console.log("loggedinUser.userName", loggedinUser.userName);
+  console.log("commentAuthor", commentAuthor);
 
   const handleDeletePlant = async () => {
     const token = getToken();
@@ -73,7 +85,6 @@ function Pdp() {
 
       const urlencoded = new URLSearchParams();
       urlencoded.append("text", inputText /* && newComment.text */);
-      console.log("newComment", newComment.text);
       console.log("newComment.text", newComment.text);
 
       const requestOptions2 = {
@@ -106,6 +117,44 @@ function Pdp() {
     }
   };
 
+  //delete comment
+  const handleDeleteComment = async (comment) => {
+    const token = getToken();
+
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+      myHeaders.append("Content-Type", "application/json");
+
+      console.log("comment._id :>> ", comment._id);
+
+      const raw = JSON.stringify({
+        commentId: comment._id,
+      });
+
+      if (!comment._id) {
+        console.log("comment._id is undefined");
+        return;
+      }
+
+      const requestOptions3 = {
+        method: "DELETE",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      const response = await fetch(
+        `http://localhost:5000/api/plants/${_id}/comments`,
+        requestOptions3
+      );
+      const result = await response.json();
+      setUpdatedComments(result.plant.comments);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   useEffect(() => {
     if (plant) {
       setSelectedPlant(plant.plantById[0]);
@@ -113,110 +162,149 @@ function Pdp() {
   }, [plant]);
 
   return (
-    <div>
+    <>
       {isLoading ? (
         <p>Loading...</p>
-      ) : selectedPlant ? (
-        <>
-          <div className="container">
-            {loggedinUser.email === selectedPlant.userEmail && (
-              <>
-                <Button variant="danger" onClick={() => setShowModal(true)}>
-                  Delete Plant
-                </Button>
-                <Modal show={showModal} onHide={() => setShowModal(false)}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>Confirm Delete</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    Are you sure you want to delete this plant?
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button
-                      variant="secondary"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Cancel
-                    </Button>
-                    <Button variant="danger" onClick={handleDeletePlant}>
-                      Delete
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-              </>
-            )}
-
-            <h1 className=" h1-pdp text-center"> {selectedPlant.name}</h1>
-            <div className="product-img-container">
-              <img
-                className="container d-flex justify-content-center"
-                src={selectedPlant.image}
-                style={{ height: "30em", width: "auto" }}
-              ></img>
-            </div>
-            <p>Author: {selectedPlant.userName}</p>
-            <p>Description: {selectedPlant.description}</p>
-            <h5>Germinating month: {selectedPlant.germinating_season} </h5>
-            <h5>Harvest month: {selectedPlant.harvest} </h5>
-            <div className=" align-items-baseline">
-              {/* <Button variant="primary">Save Me</Button> */}
-            </div>
-          </div>
-
-          <h2 className="text-center">
-            {" "}
-            Post a comment or a question for : {selectedPlant.name}
-          </h2>
-          <div className="comment-section container">
-            {selectedPlant.comments &&
-              selectedPlant.comments.map((comment, i) => {
-                return (
-                  <div className="comment text-center" /* key={i} */>
-                    <p> Author: {comment.author}</p>
-                    <img
-                      src={comment.authorPicture}
-                      alt="Avatar"
-                      style={{
-                        width: "100px",
-                        borderRadius: "50%",
-                        aspectRatio: "1/1",
-                        objectFit: "cover",
-                      }}
-                    ></img>
-                    <p> Comment: {comment.text}</p>
-                  </div>
-                );
-              })}
-            <div className="text-center input-comment">
-              <input
-                type="text"
-                className="text-center"
-                value={inputText}
-                onChange={handleInputChange}
-              />
-
-              {loggedinUser ? (
-                <button
-                  className="comment-button"
-                  onClick={() => handleComment()}
-                >
-                  Submit
-                </button>
-              ) : (
-                <Link to="/login">
-                  <button> login first</button>
-                </Link>
-              )}
-            </div>
-          </div>
-          {/* <Comments id={product.id} /> */}
-        </>
       ) : (
-        <p>No plant found</p>
+        selectedPlant && (
+          <>
+            <div className="container">
+              {loggedinUser.email === selectedPlant.userEmail && (
+                <>
+                  <Button variant="danger" onClick={() => setShowModal(true)}>
+                    Delete Plant
+                  </Button>
+                  <Modal show={showModal} onHide={() => setShowModal(false)}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Confirm Delete</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      Are you sure you want to delete this plant?
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button
+                        variant="secondary"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Cancel
+                      </Button>
+                      <Button variant="danger" onClick={handleDeletePlant}>
+                        Delete
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </>
+              )}
+
+              <h1 className=" h1-pdp text-center"> {selectedPlant.name}</h1>
+              <div className="product-img-container">
+                <img
+                  className="container d-flex justify-content-center"
+                  src={selectedPlant.image}
+                  style={{ height: "30em", width: "auto" }}
+                ></img>
+              </div>
+              <p>Author: {selectedPlant.userName}</p>
+              <p>Description: {selectedPlant.description}</p>
+              <h5>Germinating month: {selectedPlant.germinating_season} </h5>
+              <h5>Harvest month: {selectedPlant.harvest} </h5>
+              <div className=" align-items-baseline">
+                {/* <Button variant="primary">Save Me</Button> */}
+              </div>
+            </div>
+
+            <h2 className="text-center">
+              {" "}
+              Post a comment or a question for : {selectedPlant.name}
+            </h2>
+            <div className="comment-section container">
+              {selectedPlant.comments &&
+                selectedPlant.comments.map((comment, i) => {
+                  return (
+                    <div className="comment text-center" key={comment._id}>
+                      <p> Author: {comment.author}</p>
+                      <img
+                        src={comment.authorPicture}
+                        alt="Avatar"
+                        style={{
+                          width: "100px",
+                          borderRadius: "50%",
+                          aspectRatio: "1/1",
+                          objectFit: "cover",
+                        }}
+                      ></img>
+                      <p> Comment: {comment.text}</p>
+
+                      {comment.author === loggedinUser.userName ? (
+                        <>
+                          <Button
+                            variant="danger"
+                            onClick={() => setShowModal(true)}
+                          >
+                            Delete Comment
+                          </Button>
+                          <Modal
+                            show={showModal}
+                            onHide={() => setShowModal(false)}
+                          >
+                            <Modal.Header closeButton>
+                              <Modal.Title>Confirm Delete</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                              Are you sure you want to delete your comment?
+                            </Modal.Body>
+                            <Modal.Footer>
+                              <Button
+                                variant="secondary"
+                                onClick={() => setShowModal(false)}
+                              >
+                                Cancel
+                              </Button>
+                              <Button
+                                variant="danger"
+                                onClick={() => handleDeleteComment(comment)}
+                              >
+                                Delete
+                              </Button>
+                            </Modal.Footer>
+                          </Modal>
+                        </>
+                      ) : null}
+                    </div>
+                  );
+                })}
+              <div className="text-center input-comment">
+                <input
+                  type="text"
+                  className="text-center"
+                  value={inputText}
+                  onChange={handleInputChange}
+                />
+
+                {loggedinUser ? (
+                  <button
+                    className="comment-button"
+                    onClick={() => handleComment()}
+                  >
+                    Submit
+                  </button>
+                ) : (
+                  <Link to="/login">
+                    <button> login first</button>
+                  </Link>
+                )}
+              </div>
+            </div>
+
+            {/* ) : (
+       <p>No plant found</p>
+       )}
+       {/* {error && <Navigate to="*" />} */}
+            {/* {error && <p>No plant with this id</p>} */}
+          </>
+        )
       )}
-      {/* {error && <Navigate to="*" />} */}
-      {error && <p>No plant with this id</p>}
-    </div>
+    </>
   );
 }
 
